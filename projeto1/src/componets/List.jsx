b4e4e8d12261495a24e90ce8266746760bfbcda1
NathanDalbert <dalbert.nathan@gmail.com>
2024-09-16 19:27:26 -0300

    fix: adicionando tipos aos props

--- projeto1/src/componets/List.jsx
+++ projeto1/src/componets/List.jsx
@@ -4,9 +4,10 @@ function List(){
         <>
             <h1>Minha Lista</h1>
             <ul >
-                <Item marca = "Ferarri"/>
-                <Item marca = "Renault"/>
-                <Item marca = "Fiat"/>
+                <Item marca = "Ferarri"ano_lancamento = {1985}/>
+                <Item marca = "Renault" ano_lancamento = {2000}/>
+                <Item marca = "Fiat" ano_lancamento = {1998}/>
+                <Item/>
             </ul>
         </>
     )
