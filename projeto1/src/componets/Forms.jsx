5b8dfc952a1ca521052cd9328a66b2c9b05fee3d
NathanDalbert <dalbert.nathan@gmail.com>
2024-10-17 18:04:08 -0300

    feat(useStates): implementar useStates

--- projeto1/src/componets/Forms.jsx
+++ projeto1/src/componets/Forms.jsx
@@ -1,16 +1,36 @@
+import { useState } from "react"
 function Form(){
 
     function cadastrarUsuario(e){
         e.preventDefault()
-        console.log("Usuario cadastrado")
+        console.log(name)
+        console.log(`Usuario :${name} foi cadastrado com a senha : ${password}`)
     }
+
+    const [name, setName] =useState()
+    const [password,setPassword] = useState()
     return(
 
         <>
         <h1>Meu cadastro</h1>
         <  form onSubmit={cadastrarUsuario}>
             <div>
-                <input type="text" placeholder="Digite seu nome: "/>
+                <label htmlFor="name">Nome:</label>
+                <input 
+                type="text" 
+                id = "name"
+                name = "name"
+                placeholder="Digite seu nome: "
+                onChange={(e) => setName(e.target.value)}/>
+            </div>
+            <div>
+                <label htmlFor="password">Senha :</label>
+                <input 
+                type="password"
+                id = "password"
+                name ="password"
+                placeholder="Digite a senha:"
+                onChange={(e) => setPassword(e.target.value)}/>
             </div>
             <div>
                 <input type="submit" value= "Cadastrar" />
